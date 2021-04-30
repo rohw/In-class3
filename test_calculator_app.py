a795951a3b4fb40128e7a2c3255b3ed4080724c1
Wonjun Roh <rohw@oregonstate.edu>
2021-04-30 09:02:29 +0900

    add addition testcase

--- test_calculator_app.py
+++ test_calculator_app.py
@@ -1,2 +1,11 @@
-import calculator_app
 import unittest
+import calculator_app
+
+
+class testCaseAdd(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(calculator_app.addition(5, 10), 14)
+
+
+if __name__ == "__main__":
+    unittest.main()
