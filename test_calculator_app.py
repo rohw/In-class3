c26082ddbfa5a35ae131af51965d13765b1932a0
Wonjun Roh <rohw@oregonstate.edu>
2021-04-30 09:19:47 +0900

    finished testing

--- test_calculator_app.py
+++ test_calculator_app.py
@@ -4,7 +4,16 @@ import calculator_app
 
 class testCaseAdd(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(calculator_app.addition(5, 10), 14)
+        self.assertEqual(calculator_app.addition(5, 10), 15)
+
+    def test_subtraction(self):
+        self.assertEqual(calculator_app.subtraction(5, 10), -5)
+
+    def test_multiplication(self):
+        self.assertEqual(calculator_app.multiplication(5, 10), 50)
+
+    def test_division(self):
+        self.assertEqual(calculator_app.division(10, 5), 2)
 
 
 if __name__ == "__main__":
